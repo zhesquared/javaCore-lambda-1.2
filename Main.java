01650ac33e7f1f1d396085a0ad7949287ea9c00a
Eugene Borisov <eugeni.borisov@gmail.com>
2022-09-19 22:13:59 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+}
